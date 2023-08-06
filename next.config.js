e7bde6f853282f0e0f3f6a9b7608b0bc674ead24
davidrocha9 <up201806371@fe.up.pt>
2023-08-06 18:09:11 +0100

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withPWA = require("next-pwa");
 
 const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+    target: 'serverless', // This ensures compatibility with platforms like GitHub Pages
+    // Other configurations if needed
+    images: {
+        // Set this to true to disable Image Optimization
+        unoptimized: true,
+      },
+};
