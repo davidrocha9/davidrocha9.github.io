e5829a894400c9b2c67b8c1caf908f35485765fc
davidrocha9 <up201806371@fe.up.pt>
2024-06-01 00:39:53 +0100

    Fixed document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script';
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <Script
-            strategy="afterInteractive"
+          <script
+            async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
           />
-
-          <Script id="ga-script" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
-                  `}
-          </Script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+          {/* Include other meta tags and links */}
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#7000FF" />
