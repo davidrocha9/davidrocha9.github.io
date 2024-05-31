aca4d345e1eb47b04cfd78f7e6b24628c4b1f567
davidrocha9 <up201806371@fe.up.pt>
2024-06-01 00:36:01 +0100

    Added analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,25 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script';
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+          />
+
+          <Script id="ga-script" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+                  `}
+          </Script>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#7000FF" />
